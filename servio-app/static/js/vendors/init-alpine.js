8e4520fa20f92da703ebfb77698293347187bcc1
sammykingx <sammy@sammykingx.com.ng>
2025-12-08 05:58:18 +0100

    chore: modal for account socials and profile info

--- servio-app/static/js/vendors/init-alpine.js
+++ servio-app/static/js/vendors/init-alpine.js
@@ -7,6 +7,28 @@ document.addEventListener("alpine:init", () => {
     },
   }));
   
+// Modal management
+  Alpine.store("modals", {
+  profile: false,
+  address: false,
+  socials: false,
+
+  // optional: helper methods
+  open(modalName) {
+    this[modalName] = true;
+  },
+  close(modalName) {
+    this[modalName] = false;
+  },
+  toggle(modalName) {
+    this[modalName] = !this[modalName];
+  },
+  closeAll() {
+    this.profile = false;
+    this.address = false;
+    this.socials = false;
+  }
+});
 
   // Theme (dark/light mode)
   Alpine.data("themeHandler", () => ({
@@ -19,3 +41,4 @@ document.addEventListener("alpine:init", () => {
   }));
   
 });
+
