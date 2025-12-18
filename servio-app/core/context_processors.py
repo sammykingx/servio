79ae8efcdd489f540ebd9313197fe79e25d7b463
sammykingx <sammy@sammykingx.com.ng>
2025-12-18 09:02:39 +0100

    fix: fixed TypeError bug caused in context processors

--- servio-app/core/context_processors.py
+++ servio-app/core/context_processors.py
@@ -5,16 +5,17 @@ from notifications.models.notification_channels import NotificationChannels
 def app_urlnames(request):
     # lazily creating notifications with defaults
     # if not present in model
-    channels, created = NotificationChannels.objects.get_or_create(
-            user=request.user,
-            defaults={
-                "in_app": True,
-                "email": True,
-                "web_push": False,
-                "sms": False,
-                "whatsapp": False,
-            }
-        )
+    if request.user.is_authenticated:
+        channels, created = NotificationChannels.objects.get_or_create(
+                user=request.user,
+                defaults={
+                    "in_app": True,
+                    "email": True,
+                    "web_push": False,
+                    "sms": False,
+                    "whatsapp": False,
+                }
+            )
     
     return {
         "AUTH_URLS": AuthURLNames,
