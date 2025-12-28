20b87b3f2b2c543b9ef791b2c738455f50451c0e
sammykingx <sammy@sammykingx.com.ng>
2025-12-28 03:17:13 +0100

    chore: default UI states for gigs and paymets

--- servio-app/collaboration/views/list.py
+++ servio-app/collaboration/views/list.py
@@ -5,4 +5,10 @@ from template_map.collaboration import Collabs
 
 # Create your views here.
 class CollaborationListView(LoginRequiredMixin, TemplateView):
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['collaborations'] = []
+        return context
+
     template_name = Collabs.LIST_COLLABORATIONS
